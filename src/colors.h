43a8af7509a29d8236d3f9fe13bc81bd626375f7
Edutech Blocks <108950754+EdutechBlocks@users.noreply.github.com>
2022-07-08 20:28:17 -0300

    Add files via upload

---
+++ src/colors.h
@@ -0,0 +1,31 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Default color definitions
+#define ST7735_GREY 0x5AEB
+//#define ST7735_BLACK       0x0000      /*   0,   0,   0 */
+#define ST7735_NAVY        0x000F      /*   0,   0, 128 */
+#define ST7735_DARKGREEN   0x03E0      /*   0, 128,   0 */
+#define ST7735_DARKCYAN    0x03EF      /*   0, 128, 128 */
+#define ST7735_MAROON      0x7800      /* 128,   0,   0 */
+#define ST7735_PURPLE      0x780F      /* 128,   0, 128 */
+#define ST7735_OLIVE       0x7BE0      /* 128, 128,   0 */
+#define ST7735_LIGHTGREY   0xD69A      /* 211, 211, 211 */
+#define ST7735_DARKGREY    0x7BEF      /* 128, 128, 128 */
+//#define ST7735_BLUE        0x001F      /*   0,   0, 255 */
+//#define ST7735_GREEN       0x07E0      /*   0, 255,   0 */
+//#define ST7735_CYAN        0x07FF      /*   0, 255, 255 */
+//#define ST7735_RED         0xF800      /* 255,   0,   0 */
+//#define ST7735_MAGENTA     0xF81F      /* 255,   0, 255 */
+//#define ST7735_YELLOW      0xFFE0      /* 255, 255,   0 */
+//#define ST7735_WHITE       0xFFFF      /* 255, 255, 255 */
+//#define ST7735_ORANGE      0xFDA0      /* 255, 180,   0 */
+#define ST7735_GREENYELLOW 0xB7E0      /* 180, 255,   0 */
+#define ST7735_PINK        0xFE19      /* 255, 192, 203 */ //Lighter pink, was 0xFC9F
+#define ST7735_BROWN       0x9A60      /* 150,  75,   0 */
+#define ST7735_GOLD        0xFEA0      /* 255, 215,   0 */
+#define ST7735_SILVER      0xC618      /* 192, 192, 192 */
+#define ST7735_SKYBLUE     0x867D      /* 135, 206, 235 */
+#define ST7735_VIOLET      0x915C      /* 180,  46, 226 */
+
+#endif
